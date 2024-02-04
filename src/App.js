91bf03c26876dc1f59d37ef763fac1d3ae580179
Alican Basak <alicanbasak@icloud.com>
2024-02-04 11:51:27 +0300

    re design component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React from "react";
-import ThumbsUp from "./components/Icons/ThumbsUp";
-import ThumsDown from "./components/Icons/ThumsDown";
-import SubmitIcon from "./components/Icons/SubmitIcon";
+import React, { useState } from "react";
 import NewFact from "./components/Form/NewFact";
 import FactList from "./components/Facts/FactList";
 
-const Header = ({ title }) => {
+const Header = ({ title, handleShowForm }) => {
   return (
     <header className="header">
       <div className="logo">
         <img src="logo.png" height="48" width="48" alt="Today I Learned Logo" />
         <h1>{title}</h1>
       </div>
-
-      <button className="btn btn-large btn-post">Share a fact</button>
+      <button
+        onClick={() => handleShowForm()}
+        className="btn btn-large btn-post"
+      >
+        Share a fact
+      </button>
     </header>
   );
 };
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleShowForm = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div className="container">
-      <Header title={"Today I Learned"} />
-      <NewFact />
+      <Header handleShowForm={handleShowForm} title={"Today I Learned"} />
+      <NewFact showForm={showForm} />
       <FactList />
     </div>
   );
